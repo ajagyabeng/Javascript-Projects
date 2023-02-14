4f90e4bb4a909f7e92263bf9b36eb18fecb30149
ajagyabeng <ajagyabeng@gmail.com>
2023-02-14 17:14:58 +0000

    map out js file

---
+++ app.js
@@ -0,0 +1,11 @@
+// Book Class: Represents a Book
+
+// UI Class: Handle UI Tasks(book display, removed, alerts)
+
+// Store Class: Handle Storage(Local Storage)
+
+// Event: Display Books
+
+// Event: Add Books
+
+// Event: Remove Books
