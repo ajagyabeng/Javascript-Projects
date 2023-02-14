3319ecdd244704614163fd91785b9b07b30d0d48
ajagyabeng <ajagyabeng@gmail.com>
2023-02-14 17:16:41 +0000

    create book class

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // Book Class: Represents a Book
-
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
 // UI Class: Handle UI Tasks(book display, removed, alerts)
 
 // Store Class: Handle Storage(Local Storage)
