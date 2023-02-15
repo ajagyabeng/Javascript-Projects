11a890b12c5b75c1ff3f4e12c07a83e1ea313899
ajagyabeng <ajagyabeng@gmail.com>
2023-02-15 15:43:15 +0000

    select element and add eventlistener

---
+++ Filterable List/main.js
@@ -0,0 +1,9 @@
+// Get input element
+const filterInput = document.querySelector("#filter-input");
+
+// Add eventlistener
+filterInput.addEventListener("keyup", filterNames);
+
+function filterNames() {
+  console.log(1);
+}
