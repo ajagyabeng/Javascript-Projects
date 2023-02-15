5cb742bb6ebe9b659fab23b1f6a12d04c2370c2e
ajagyabeng <ajagyabeng@gmail.com>
2023-02-15 17:05:52 +0000

    add OOP

--- Filterable List/main.js
+++ Filterable List/main.js
@@ -16,7 +16,6 @@ function filterNames() {
 
   //Loop through collection-item lis
   lis.forEach((li) => {
-    //
     let a = li.getElementsByTagName("a")[0];
 
     // Check if it matches
@@ -28,3 +27,56 @@ function filterNames() {
     }
   });
 }
+
+/*--------------------CLASSES-----------------------------*/
+
+class Contact {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Store {
+  static getContacts() {
+    let contacts;
+
+    if (localStorage.getItem("contacts") === null) {
+      contacts = [];
+    } else {
+      contacts = JSON.parse(localStorage.getItem("contacts"));
+    }
+    return contacts;
+  }
+
+  static addContact(contact) {
+    const contacts = Store.getContacts();
+
+    contacts.push(contact);
+
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }
+}
+
+class UI {
+  static clearContactField() {
+    document.querySelector("#name").value = "";
+  }
+}
+
+/*--------------------EVENT LISTENERS---------------------*/
+// add eventListener of form
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Get hold of input value
+  const name = document.querySelector("#name").value;
+
+  // validate input
+  if (name === "") {
+    alert("Please fill all fields"); // change this later
+  } else {
+    const contact = new Contact(name);
+    Store.addContact(contact);
+    UI.clearContactField();
+  }
+});
