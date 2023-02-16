771d4774cbc371a853415062e72c87a4c4c41288
ajagyabeng <ajagyabeng@gmail.com>
2023-02-16 22:45:38 +0000

    hide output cards

--- Weight Converter/main.js
+++ Weight Converter/main.js
@@ -1,6 +1,10 @@
+// Hide output cards until value is entered
+document.querySelector("#output").style.visibility = "hidden";
+
 // Event Listener: Input
 document.querySelector("#lbsInput").addEventListener("input", (e) => {
   // Get hold of value
+  document.querySelector("#output").style.visibility = "visible";
   const lbs = e.target.value;
 
   document.querySelector("#grams-output").innerHTML = lbs / 0.0022046;
