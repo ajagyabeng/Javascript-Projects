3b0a5ddfcab9b300f683306e3ae9fbc3bfbe76d6
ajagyabeng <ajagyabeng@gmail.com>
2023-02-17 00:25:26 +0000

    block all output from display

--- Weight Converter/main.js
+++ Weight Converter/main.js
@@ -1,15 +1,35 @@
-// Hide output cards until value is entered
-document.querySelector("#output").style.visibility = "hidden";
+// Hide input field until unit to convert from is selected.
+document.querySelector("#input-form").style.display = "none";
+
+// Hide output cards until unit to convert to is selected
+document.querySelector("#g-block").style.display = "none";
+document.querySelector("#kg-block").style.display = "none";
+document.querySelector("#oz-block").style.display = "none";
+document.querySelector("#lbs-block").style.display = "none";
+
+/*------------EVENT LISTENERS-----------------*/
+// Event Listener: Select unit to convert from
+document.querySelector("#unit-from").addEventListener("change", (e) => {
+  document.querySelector("#input-form").style.display = "block";
+});
+
+// Event Listener: Select unit to convert to
+document.querySelector("#unit-to").addEventListener("change", (e) => {
+  document.getElementById(`${e.target.value}-block`).style.display = "block";
+});
 
 // Event Listener: Input
 document.querySelector("#lbsInput").addEventListener("input", (e) => {
   // Get hold of value
-  document.querySelector("#output").style.visibility = "visible";
+  // document.querySelector("#output").style.visibility = "visible";
   const lbs = e.target.value;
 
+  // display gram value in output
   document.querySelector("#grams-output").innerHTML = lbs / 0.0022046;
 
+  // display kilogram value in output
   document.querySelector("#kg-output").innerHTML = lbs / 2.2046;
 
+  // display ounze value in output
   document.querySelector("#oz-output").innerHTML = lbs * 16;
 });
