63ec894bd165d314e6adab2ddf11a739860d7c65
ajagyabeng <ajagyabeng@gmail.com>
2023-02-24 00:40:52 +0000

    update selectors

--- Weight Converter/main.js
+++ Weight Converter/main.js
@@ -9,19 +9,26 @@ document.querySelector("#lbs-block").style.display = "none";
 
 /*------------EVENT LISTENERS-----------------*/
 // Event Listener: Select unit to convert from
-document.querySelector("#unit-from").addEventListener("change", (e) => {
+document.querySelector("#input-unit").addEventListener("change", (e) => {
   UI.enableOutputUnit();
   UI.checkCurrentInput(e);
 });
 
 // Event Listener: Select unit to convert to
-document.querySelector("#unit-to").addEventListener("change", (e) => {
+document.querySelector("#output-unit").addEventListener("change", (e) => {
+  UI.enableInputUnit();
+  UI.checkCurrentOutput(e);
   UI.selectOutput(e);
 });
 
-// Event Listener: Disables unit that is to be converted from
-document.querySelector("#select-unit").addEventListener("click", (e) => {
+// Event Listener: Disables selected input unit from output unit options
+document.querySelector("#select-output-unit").addEventListener("click", (e) => {
   UI.disableOutputUnit();
+});
+
+// Event Listener: Disables slected output unit from input unit options
+document.querySelector("#select-input-unit").addEventListener("click", (e) => {
+  UI.disableInputUnit();
 });
 
 // Event Listener: Input
@@ -29,7 +36,7 @@ document.querySelector("#Input").addEventListener("input", (e) => {
   // Get hold of value for calculation
   const weight = e.target.value;
 
-  const options = document.querySelectorAll(".from-option");
+  const options = document.querySelectorAll(".input-unit-option");
   options.forEach((option) => {
     if (
       option.classList.contains("current") &&
@@ -55,7 +62,10 @@ document.querySelector("#Input").addEventListener("input", (e) => {
       document.querySelector("#grams-output").innerHTML = weight * 1000;
       document.querySelector("#lbs-output").innerHTML = weight * 2.20462;
       document.querySelector("#oz-output").innerHTML = weight * 35.274;
-    } else {
+    } else if (
+      option.classList.contains("current") &&
+      option.innerHTML === "Ounces(oz)"
+    ) {
       // oz
       document.querySelector("#kg-output").innerHTML = weight / 32.274;
       document.querySelector("#lbs-output").innerHTML = weight / 16;
@@ -66,14 +76,15 @@ document.querySelector("#Input").addEventListener("input", (e) => {
 
 /*------------------------OOP: Classes-----------------------*/
 class UI {
+  /*---------------------- INPUT UNITS---------------------------*/
   static checkCurrentInput(e) {
-    /*checks if a unit has been selected before displaying input field*/
+    /*checks for currently selected input unit.*/
 
     // get the index of the newly selected input
     const selectedOptionIndex = e.target.options.selectedIndex;
 
     // check if an option already has the 'current' class then remove it before adding it to the newly selected option
-    const options = document.querySelectorAll(".from-option");
+    const options = document.querySelectorAll(".input-unit-option");
     options.forEach((option) => {
       if (option.classList.contains("current")) {
         option.classList.remove("current");
@@ -90,6 +101,47 @@ class UI {
     }
   }
 
+  // DISABLE/ENABLE INPUT UNITS
+  static enableInputUnit() {
+    /*check if an option in unit to convert to is disabled and enable it.*/
+    const toOptions = document.querySelectorAll(".input-unit-option");
+    toOptions.forEach((option) => {
+      if (option.hasAttribute("disabled")) {
+        option.disabled = false;
+      }
+    });
+  }
+
+  static disableInputUnit() {
+    const toOptions = document.querySelectorAll(".output-unit-option");
+    toOptions.forEach((option) => {
+      if (option.classList.contains("current")) {
+        const toAttribute = option.getAttribute("value");
+        // Get hold of options to convert to and set attribute to disabled
+        const fromOptions = document.querySelectorAll(".input-unit-option");
+        fromOptions.forEach((option) => {
+          if (option.getAttribute("value") === toAttribute) {
+            option.disabled = true;
+          }
+        });
+      }
+    });
+  }
+
+  /*----------------------OUTPUT UNITS---------------------------*/
+  static checkCurrentOutput(e) {
+    const selectedOptionIndex = e.target.options.selectedIndex;
+    const options = document.querySelectorAll(".output-unit-option");
+    options.forEach((option) => {
+      if (option.classList.contains("current")) {
+        option.classList.remove("current");
+        options[selectedOptionIndex].classList.add("current");
+      } else {
+        options[selectedOptionIndex].classList.add("current");
+      }
+    });
+  }
+
   static selectOutput(e) {
     /*Removes card when a different unit is selected*/
     const outputCards = document.querySelectorAll(".output-card");
@@ -102,9 +154,11 @@ class UI {
     }
   }
 
+  // ENABLE/DISABLE OUTPUT UNITS
+
   static enableOutputUnit() {
     /*check if an option in unit to convert to is disabled and enable it.*/
-    const toOptions = document.querySelectorAll(".to-option");
+    const toOptions = document.querySelectorAll(".output-unit-option");
     toOptions.forEach((option) => {
       if (option.hasAttribute("disabled")) {
         option.disabled = false;
@@ -113,12 +167,12 @@ class UI {
   }
 
   static disableOutputUnit() {
-    const fromOptions = document.querySelectorAll(".from-option");
+    const fromOptions = document.querySelectorAll(".input-unit-option");
     fromOptions.forEach((option) => {
       if (option.classList.contains("current")) {
         const fromAttribute = option.getAttribute("value");
         // Get hold of options to convert to and set attribute to disabled
-        const toOptions = document.querySelectorAll(".to-option");
+        const toOptions = document.querySelectorAll(".output-unit-option");
         toOptions.forEach((option) => {
           if (option.getAttribute("value") === fromAttribute) {
             option.disabled = true;
@@ -128,3 +182,8 @@ class UI {
     });
   }
 }
+
+/*
+TODOS
+1. 
+*/
