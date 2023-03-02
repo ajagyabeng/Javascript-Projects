5828a323131fe2a09e717bb43c0c8c0f1addad14
ajagyabeng <ajagyabeng@gmail.com>
2023-03-02 12:57:58 +0000

    Create add_contact.js

---
+++ Contact List/static/js/add_contact.js
@@ -0,0 +1,25 @@
+import { UI } from "../js/ui.js";
+import { Store } from "../js/contact.js";
+
+// Event Listener: Add Contact to Database
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Get hold of input value
+  const name = document.querySelector("#name").value;
+  const phone = document.querySelector("#phone").value;
+  const email = document.querySelector("#email").value;
+
+  // validate input
+  if (name === "" || phone === "") {
+    alert("Please fill all fields"); // change this later
+  } else {
+    const contact = new Contact(name, phone, email);
+
+    //Hide Contact form
+    UI.hideContactForm();
+    //Store contact to database
+    Store.addContact(contact);
+    UI.clearContactField();
+  }
+});
