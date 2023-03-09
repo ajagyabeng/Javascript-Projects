e790de0990071613dcbd10c92f8e022b94fbd49c
ajagyabeng <ajagyabeng@gmail.com>
2023-03-09 10:17:01 +0000

    Add view_contact and format code

--- Contact List/app.py
+++ Contact List/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import setup_db, create_tables, Contact, add_to_db, delete_from_db
+from models import setup_db, create_tables, Contact, add_to_db, delete_from_db, edit_in_db
 from create_app import create_app
 from forms import ContactForm
 from dotenv import find_dotenv, load_dotenv
 import os
+import json
 
 # Find environment variables
 dotenv_path = find_dotenv()
@@ -36,6 +37,15 @@ def format_contacts(contacts):
     return contact_list
 
 
+def format_contact(person):
+    """Formats a contact from the database. Returns an object"""
+    contact = {"name": person.name,
+               "phone": person.phone,
+               "email": person.email,
+               "id": person.id}
+    return json.loads(json.dumps(contact))
+
+
 @app.route("/contacts")
 def get_contacts():
     """
@@ -50,28 +60,57 @@ def get_contacts():
 
 @app.route("/add", methods=["GET", "POST"])
 def add_contact():
+    """"""
     form = ContactForm()
     if request.method == "POST" and form.validate_on_submit():
         data = request.form.to_dict()
         try:
             add_to_db(data)
-            print(data)
             phone = data["phone"]
-            return redirect(url_for("view_contact", phone=phone))
+            person = Contact.query.filter_by(phone=phone).first()
+            return redirect(url_for("view_contact", id=person.id))
         except:
             return {"message": "Unable to add to database"}
     return render_template("add_contact.html", form=form)
 
 
-@app.route("/person")
-def view_contact():
-    phone = request.args.get("phone")
+@app.route("/person/<int:id>", methods=["GET", "POST"])
+def view_contact(id):
+    """"""
+    # phone = request.args.get("phone")
+    person = Contact.query.get(id)
+    edit_form = ContactForm(
+        name=person.name,
+        phone=person.phone,
+        email=person.email
+    )
+    if request.method == "POST" and edit_form.validate_on_submit():
+        data = request.form.to_dict()
+        edit_in_db(data, person)
+        return {"contact": format_contact(person)}
+    else:
+        print(edit_form.errors)
+    return render_template("person.html", contact=format_contact(person), form=edit_form)
+
+
+@app.route("/edit/<int:phone>", methods=["GET", "POST"])
+def edit_contact(phone):
     person = Contact.query.filter_by(phone=phone).first()
-    return render_template("person.html", contact=person)
+    edit_form = ContactForm(
+        name=person.name,
+        phone=person.phone,
+        email=person.email
+    )
+    if request.method == "POST" and edit_form.validate_on_submit():
+        data = request.form.to_dict()
+        edit_in_db(data, person)
+        return redirect(url_for("view_contact", phone=phone))
+    return render_template("person.html", form=edit_form, is_edit=True, contact=person)
 
 
 @app.route("/delete")
 def delete():
+    """"""
     phone = request.args.get("phone")
     contact = Contact.query.filter_by(phone=phone).first()
     delete_from_db(contact)
