528c3eb6bf9819f219e81035f38f4d8787a9df9a
ajagyabeng <ajagyabeng@gmail.com>
2023-03-09 13:40:03 +0000

    Update TODOS

--- Contact List/static/js/main.js
+++ Contact List/static/js/main.js
@@ -15,12 +15,9 @@ document
 /*
 TODOS
 UPDATE CONTACT
-DELETE CONTACT
 Add an 'unable to add html' to display when a contact wasnt added
 1. When adding new contact, check if the exact name already exists(warn to make changes to avoid confusion)
 2. When adding new contact, check if the exact number already exist in contacts(show number and name which it is stored to)
-3. Add pop up when name is clicked(displays Name, Phone, Email )
-4. Connect to server
 4i. Add labels(Friends, family, work, school, etc.)
 5. More actions(Hide, delete)
 6. Add contact(add country calling code to form) - countryinfo module in python
