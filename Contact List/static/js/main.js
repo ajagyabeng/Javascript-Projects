fd0125660aca30a58efc8f9dc1f5b9de3415ce08
ajagyabeng <ajagyabeng@gmail.com>
2023-03-09 18:24:57 +0000

    Update TODOS

--- Contact List/static/js/main.js
+++ Contact List/static/js/main.js
@@ -15,10 +15,12 @@ document
 /*
 TODOS
 UPDATE CONTACT
+HIDE CONTACTS AND ONLY DISPLAY THEM WHEN THEY COME INTO VIEW FROM SCROLLING AND HIDE THEM WHEN THEY SCROLL PASS THE VIEWPORT
 Add an 'unable to add html' to display when a contact wasnt added
 1. When adding new contact, check if the exact name already exists(warn to make changes to avoid confusion)
 2. When adding new contact, check if the exact number already exist in contacts(show number and name which it is stored to)
 4i. Add labels(Friends, family, work, school, etc.)
 5. More actions(Hide, delete)
 6. Add contact(add country calling code to form) - countryinfo module in python
+7. Implement flask cors, oauth, create users, etc.
 */
