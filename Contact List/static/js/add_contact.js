12abf3caa8084902d5dc8ee1b1d35a3443c18626
ajagyabeng <ajagyabeng@gmail.com>
2023-03-09 10:23:56 +0000

    Refactor new contact code

--- Contact List/static/js/add_contact.js
+++ Contact List/static/js/add_contact.js
@@ -2,22 +2,6 @@ import { UI } from "../js/ui.js";
 import { Store, Contact } from "../js/contact.js";
 
 /*Event Listener: Add Contact to Database*/
-// document.querySelector("#contact-form").addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   // Get hold of input value
-//   const name = document.querySelector("#name").value;
-//   const phone = document.querySelector("#phone").value;
-//   const email = document.querySelector("#email").value;
-
-//   // validate input
-//   if (name === "" || phone === "") {
-//     alert("Please fill all fields"); // change this later
-//   } else {
-//     const contact = new Contact(name, phone, email);
-
-//     //Store contact to database
-//     Store.addContact(contact);
-//     UI.clearContactField();
-//   }
-// });
+document
+  .querySelector("#contact-form")
+  .addEventListener("submit", Store.getNewContact(e));
