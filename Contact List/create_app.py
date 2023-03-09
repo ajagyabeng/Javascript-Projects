5ac8f5efff0ac4ecd08455e05d95a0aa7eb89e6c
ajagyabeng <ajagyabeng@gmail.com>
2023-03-09 13:31:49 +0000

    Initialize app with csrf protection

--- Contact List/create_app.py
+++ Contact List/create_app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect()
 
 
 def create_app(secret_key):
     """Instantiates a flask app"""
     app = Flask(__name__)
     app.config["SECRET_KEY"] = secret_key
+    csrf.init_app(app)
     return app
